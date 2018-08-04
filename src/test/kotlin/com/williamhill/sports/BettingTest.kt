9dbcbdf880b4d1aa6a6e2e38eb48e29f5109bb72
Borys Boral <borys.boral@gmail.com>
2018-08-04 18:21:32 +0200

    Adding dependencies for selenium and cucumber.

---
+++ src/test/kotlin/com/williamhill/sports/BettingTest.kt
@@ -0,0 +1,11 @@
+package com.williamhill.sports
+
+import org.junit.Test
+
+class BettingTest {
+
+    @Test
+    fun betTest() {
+
+    }
+}
--- src/test/kotlin/com/williamhill/sports/HelloTest.kt
+++
@@ -1,5 +0,0 @@
-package com.williamhill.sports
-
-class HelloTest {
-
-}
